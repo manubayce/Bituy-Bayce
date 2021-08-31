22c02e81c4e1f4ce58d688c1f03627c8f6ef5c66
ManuelBayce <manuelbayce24@gmail.com>
2021-08-30 23:42:32 -0300

    React App con CLI

--- src/App.js
+++ src/App.js
@@ -2,6 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const numeros = [1, 2, 3, 4]
+  console.log(numeros)
+
+  const nuevoNumeros = [...numeros, 5, 6]
+  console.log(nuevoNumeros)
+
+  const auto = {
+    tipo: "vehiculo",
+    marca: "BMW",
+    modelo: "d15",
+    año: 2018
+  }
+
+  console.log(auto)
+
+  const nuevoAuto = {
+    ...auto,
+    año: 2019
+  }
+
+  console.log(nuevoAuto)
+
   return (
     <div className="App">
       <header className="App-header">
