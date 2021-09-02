5cb36b87977ae1e642ef2a13091981f065d3aa35
ManuelBayce <manuelbayce24@gmail.com>
2021-09-01 23:54:50 -0300

    Menu v1

--- src/App.js
+++ src/App.js
@@ -1,46 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from "./Navbar"
 import './App.css';
 
 function App() {
-
-  const numeros = [1, 2, 3, 4]
-  console.log(numeros)
-
-  const nuevoNumeros = [...numeros, 5, 6]
-  console.log(nuevoNumeros)
-
-  const auto = {
-    tipo: "vehiculo",
-    marca: "BMW",
-    modelo: "d15",
-    año: 2018
-  }
-
-  console.log(auto)
-
-  const nuevoAuto = {
-    ...auto,
-    año: 2019
-  }
-
-  console.log(nuevoAuto)
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/MenuItems.js
@@ -0,0 +1,32 @@
+export const MenuItems = [
+    {
+        title:'Inicio',
+        url: '#',
+        cName: 'nav-links'
+    },
+    
+    {
+        title:'Categorias',
+        url: '#',
+        cName: 'nav-links'
+    },
+
+    {
+        title:'Nosotros',
+        url: '#',
+        cName: 'nav-links'
+    },
+
+    {
+        title:'Contacto',
+        url: '#',
+        cName: 'nav-links'
+    },
+
+    {
+        title:'Preguntas',
+        url: '#',
+        cName: 'nav-links'
+    },
+
+]
